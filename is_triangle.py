27b8a91a4027824d2a0f94ab668666a99fd195fb
RyanHankss <rhanks@mchenry.edu>
2016-09-18 16:43:17 -0500

    Prompts the user and calculates

--- is_triangle.py
+++ is_triangle.py
@@ -11,4 +11,16 @@ def is_triangle(x, y, z):
         print("Yes")
 
 
-is_triangle(5, 2, 4)
+side_1 = raw_input("Find out if three lengths can make a triangle. Enter first length.\n")
+
+int(side_1)
+
+side_2 = raw_input("Second length. \n")
+
+int(side_2)
+
+side_3 = raw_input("Third length. \n")
+
+int(side_3)
+
+is_triangle(side_1, side_2, side_3)
